44a519bc13aa943acce10c54217f945c183ed137
Tom Samstag <github@modtwo.com>
2012-07-02 22:14:06 -0700

    Read the source disk partition table

--- copy_gentoo_system.pl
+++ copy_gentoo_system.pl
@@ -8,11 +8,40 @@ use Cwd 'abs_path';
 
 my %objects;
 
-use constant PREFIX => '/root/copy_gentoo_system/test';
+use constant PREFIX => $ARGV[0];
 use constant DRYRUN => 1;
+use constant SOURCE => '/dev/sda';
 
 # abs_path('/dev/root')
 
+if (@ARGV != 1)
+{
+	print "Usage: $0 device\n";
+	exit 0;
+}
+
+if (! -b PREFIX)
+{
+	print PREFIX." not a block device!\n";
+#	exit 1;
+}
+
+# read the partition table
+my $partition_table;
+my $last_partition;
+open PARTITION, "sfdisk -d '".SOURCE."'|";
+while (<PARTITION>)
+{
+	if (/^([^ ]+) : .* size=\s*(\d+),/ && $2)
+	{
+		$last_partition = $1;
+	}
+	$partition_table .= $_;
+}
+$partition_table =~ s/($last_partition : .* size=)\s*\d+,/$1+,/m;
+
+
+exit;
 for my $contentsFile (</var/db/pkg/*/*/CONTENTS>)
 {
 	open H, "<$contentsFile" or die;
