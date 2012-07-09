af52984558828471656123e4418a1245160541db
Tom Samstag <github@modtwo.com>
2012-07-08 23:52:32 -0700

    copy the MBR and the /boot partition

--- copy_gentoo_system.pl
+++ copy_gentoo_system.pl
@@ -6,80 +6,96 @@ use feature 'say';
 use Data::Dumper;
 use Cwd 'abs_path';
 use Digest::MD5::File qw( file_md5_hex );
+use File::Temp qw( tempdir );
 
 my %objects;
 
 use constant PREFIX => $ARGV[0];
 use constant DRYRUN => 1;
 use constant SOURCE => '/dev/sda';
-use constant SOURCE_BOOT => '/dev/sda1';
-use constant SOURCE_ROOT => '/dev/sda2';
+#use constant SOURCE_BOOT => '/dev/sda1';
+#use constant SOURCE_ROOT => '/dev/sda2';
 
-# abs_path('/dev/root')
+if (@ARGV != 1)
+{
+	print "Usage: $0 device\n";
+	exit 0;
+}
 
-#if (@ARGV != 1)
-#{
-#	print "Usage: $0 device\n";
-#	exit 0;
-#}
-#
-#if (! -b PREFIX)
-#{
-#	print PREFIX." not a block device!\n";
-##	exit 1;
-#}
-#
-## read the partition table
-#my $total_sectors;
-#open PARTITION, "fdisk -l '".PREFIX."'|";
-#while (<PARTITION>)
-#{
-#	$total_sectors = $1 if $_ =~ /, total (\d+) sectors/;
-#}
-#close PARTITION;
-#die "Could not get total sectors on source disk" unless($total_sectors);
-#
-#my $partition_table;
-#my $last_partition;
-#open PARTITION, "sfdisk -d '".SOURCE."'|";
-#while (<PARTITION>)
-#{
-#	if (/^([^ ]+) : .* size=\s*(\d+),/ && $2)
-#	{
-#		$last_partition = $1;
-#	}
-#	$partition_table .= $_;
-#}
-#close PARTITION;
-#$partition_table =~ s/($last_partition : start=\s*)(\d+), size=\s*\d+,/"$1$2, size=".($total_sectors-$2).","/me;
-## I don't think this is necessary, but I'm really scared of having the source in the partiion table
-#$partition_table =~ s/${\(SOURCE)}/${\(PREFIX)}/gm;
-#
-#print "---------- BEGIN PARTITION TABLE ----------\n$partition_table\n---------- END PARTITION TABLE ----------\n";
-#open SFDISK, "|sfdisk ".PREFIX;
-#print SFDISK $partition_table;
-#close SFDISK;
-#
-## give udev a chance to create partition devices
-#sleep 2;
-#
-## format the partitions
-#open FSTAB, '</etc/fstab';
-#while(<FSTAB>)
-#{
-#	chomp;
-#	s/#.*//;
-#	next unless my ($partition, $mountpoint, $type) = (/^\s*(\S+)\s+(\S+)\s+(\S+).*/);
-#	next unless substr($partition, 0, length(SOURCE)) eq SOURCE;
-#	my $newpartition = $partition;
-#	substr($newpartition, 0, length(SOURCE)) = PREFIX;
-#	print "Formatting $newpartition for $partition of type $type at $mountpoint\n";
-#	die "Unable to find mkfs for type $type" unless -x "/sbin/mkfs.$type";
-#	say "Calling: /sbin/mkfs.$type $newpartition";
-#	system "/sbin/mkfs.$type", $newpartition;
-#}
-#
-#exit;
+if (! -b PREFIX)
+{
+	print PREFIX." not a block device!\n";
+#	exit 1;
+}
+
+# copy the MBR
+system "dd", "if=".SOURCE, "of=".PREFIX, "bs=512", "count=1";
+
+# read the partition table
+my $total_sectors;
+open PARTITION, "fdisk -l '".PREFIX."'|";
+while (<PARTITION>)
+{
+	$total_sectors = $1 if $_ =~ /, total (\d+) sectors/;
+}
+close PARTITION;
+die "Could not get total sectors on source disk" unless($total_sectors);
+
+my $partition_table;
+my $last_partition;
+open PARTITION, "sfdisk -d '".SOURCE."'|";
+while (<PARTITION>)
+{
+	if (/^([^ ]+) : .* size=\s*(\d+),/ && $2)
+	{
+		$last_partition = $1;
+	}
+	$partition_table .= $_;
+}
+close PARTITION;
+$partition_table =~ s/($last_partition : start=\s*)(\d+), size=\s*\d+,/"$1$2, size=".($total_sectors-$2).","/me;
+# I don't think this is necessary, but I'm really scared of having the source in the partiion table
+$partition_table =~ s/${\(SOURCE)}/${\(PREFIX)}/gm;
+
+print "---------- BEGIN PARTITION TABLE ----------\n$partition_table\n---------- END PARTITION TABLE ----------\n";
+open SFDISK, "|sfdisk ".PREFIX;
+print SFDISK $partition_table;
+close SFDISK;
+
+# give udev a chance to create partition devices
+sleep 2;
+
+# format the partitions
+open FSTAB, '</etc/fstab';
+my $new_boot_partition;
+while(<FSTAB>)
+{
+	chomp;
+	s/#.*//;
+	next unless my ($partition, $mountpoint, $type) = (/^\s*(\S+)\s+(\S+)\s+(\S+).*/);
+	next unless substr($partition, 0, length(SOURCE)) eq SOURCE;
+	my $newpartition = $partition;
+	substr($newpartition, 0, length(SOURCE)) = PREFIX;
+	print "Formatting $newpartition for $partition of type $type at $mountpoint\n";
+	die "Unable to find mkfs for type $type" unless -x "/sbin/mkfs.$type";
+	say "Calling: /sbin/mkfs.$type $newpartition";
+	system "/sbin/mkfs.$type", $newpartition;
+	$new_boot_partition = $newpartition if($mountpoint eq '/boot');
+}
+
+die "Unable to determine boot partition" unless($new_boot_partition);
+
+my $tmp_mount = tempdir();
+say "Using temporary mount point: $tmp_mount";
+
+# copy boot partition
+system "mount", "-o", "ro", "/boot";
+system "mount", $new_boot_partition, $tmp_mount;
+system "rsync", "-aH", "/boot", $tmp_mount."/.";
+system "umount", "/boot";
+system "umount", $tmp_mount;
+
+exit;
 
 for my $contentsFile (</var/db/pkg/*/*/CONTENTS>)
 {
